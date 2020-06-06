ff9426a5eef43f47108e9400e78b22a24275e3f0
Blake Ingenthron <blakeingenthron@gmail.com>
2020-06-06 16:40:30 -0500

    Added responsiveness

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,10 +1,19 @@
 import PropTypes from 'prop-types';
 import React from 'react';
+import Tilt from '../images/splash-page-bg-2.jpg';
 
 function Layout({ children }) {
   return (
-    <div className="flex flex-col min-h-screen font-sans text-gray-900">
-      <main className="flex-1 w-full max-w-4xl px-4 py-8 mx-auto md:px-8 md:py-16">
+    <div
+      style={{
+        backgroundImage: `url(${Tilt})`,
+        backgroundRepeat: 'no-repeat',
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+      }}
+      className="flex-col flex-1 min-h-screen font-sans"
+    >
+      <main className="flex items-center justify-center w-full h-screen max-w-4xl px-4 py-8 mx-auto md:px-8 md:py-16">
         {children}
       </main>
     </div>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,22 +1,34 @@
 import React from 'react';
 
+import Layout from '../components/layout';
 import SEO from '../components/seo';
-//import catAndHumanIllustration from '../images/client-pick.png';
-import catAndHumanIllustration from '../images/splash-page.jpg';
+import catAndHumanIllustration from '../images/client-pick-3.png';
 
 function IndexPage() {
   return (
-    <>
+    <Layout>
       <SEO
         keywords={[`gatsby`, `tailwind`, `react`, `tailwindcss`]}
         title="Home"
       />
-      <img
-        alt="Cat and human sitting on a couch"
-        className="block w-full h-full mx-auto"
-        src={catAndHumanIllustration}
-      />
-    </>
+
+      <section className="text-center ">
+        <img
+          alt="Cat and human sitting on a couch"
+          className="block w-3/4 mx-auto mb-8 md:w-1/2"
+          src={catAndHumanIllustration}
+        />
+        <h2 className="p-3 mb-4 text-4xl font-thin text-white ">
+          website coming soon!
+        </h2>
+        <p className="p-3 mb-4 text-xl font-thin text-white ">
+          contact us:{' '}
+          <a style={{ color: '#a7d056' }} href="mailto:kelloggfcinfo@gmail.com">
+            kelloggfcinfo@gmail.com
+          </a>
+        </p>
+      </section>
+    </Layout>
   );
 }
 
